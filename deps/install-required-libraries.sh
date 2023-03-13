bd1bd5662ab5f58048692611a77a3333386c3ec7
Alexander Shavrov <alexander.shavrov@outlook.com>
2023-03-13 20:12:16 +0300

    Dependencies updated

--- deps/install-required-libraries.sh
+++
@@ -1,6 +0,0 @@
-cd ".."
-mkdir "lib"
-# shellcheck disable=SC2164
-cd "./lib"
-git clone https://github.com/wingmann/alef-core.git
-git clone https://github.com/wingmann/julianah.git
--- install-dependencies.cmd
+++ install-dependencies.cmd
@@ -2,20 +2,18 @@
 echo Installing dependencies...
 
 if not exist ".\lib" (
-    cd ".."
     mkdir "lib"
-    
 )
 
 cd ".\lib"
 
-if not exist ".\lib\alef-core" (
+if not exist ".\alef-core" (
     git clone https://github.com/wingmann/alef-core.git
 ) else (
     echo alef was already installed
 )
 
-if not exist ".\lib\julianah" (
+if not exist ".\julianah" (
     git clone https://github.com/wingmann/julianah.git
 ) else (
     echo julianah was already installed
